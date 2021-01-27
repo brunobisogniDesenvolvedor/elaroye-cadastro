bc163ac913371b729722b5ab3b6081b56bb92775
bruno de a. bisogni <brunobisognidev@gmail.com>
2021-01-27 12:38:32 -0300

    erro no namespace da migrations

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using elaroye_cadastro.Models; 
-using System.DateTime;
+
 
 namespace elaroye_cadastro.Data
 {
